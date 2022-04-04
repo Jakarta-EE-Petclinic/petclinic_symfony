0e0439872b89d6aaf59f77bc815ae17278d30489
thomaswoehlke <thomas.woehlke@gmail.com>
2022-04-04 23:14:05 +0200

    work

--- src/Entity/Pettype.php
+++ src/Entity/Pettype.php
@@ -2,11 +2,11 @@
 
 namespace App\Entity;
 
-
+//use App\Repository\PettypeRepository
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass=PettypeRepository::class)
  */
 class Pettype
 {
